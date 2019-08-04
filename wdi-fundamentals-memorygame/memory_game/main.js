602b12b5f7cdcb47f27ddd3c1d3fb1969d2f59eb
Paritosh Dhebar <git config --global user.email paritosh.dhebar@gmail.com>
2019-08-04 16:59:36 +1000

    'created game logic'

--- wdi-fundamentals-memorygame/memory_game/main.js
+++ wdi-fundamentals-memorygame/memory_game/main.js
@@ -1,17 +1,18 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-
-if (cardsInPlay.length === 2) {
+function checkForMatch () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You've found a match");
+  console.log("You've found a match");
   } else {
-    alert("Try Again");
-  }
+    console.log("Sorry, Try Again");
 }
+}
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+checkForMatch();
+    }
+flipCard (0);
+flipCard (2);
